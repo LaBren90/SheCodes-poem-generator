a0cc12e69eac353eaa5a9f62cacf314e23934fc3
LaBren90 <101529095+LaBren90@users.noreply.github.com>
2026-03-16 13:45:34 +0000

    api integration with AI, imported axios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
+  let promptInput = document.querySelector("#user-prompt");
+  let apiKey = "a9f2b6a7f83210eb03od0ab344b8t656";
+  let prompt = `User instructions: Generate a poem about ${promptInput.value}`;
+  let context =
+    "You are a poet who is great at making up 4-line poems from a user prompt. Be polite and follow the user instructions to create your poem in basic HTML";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
   new Typewriter("#poem", {
     strings: "Watch your poem appear here...",
     autoStart: true,
